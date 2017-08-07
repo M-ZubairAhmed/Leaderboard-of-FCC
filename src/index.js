5f1a6a24dd63c1c8472f7d6d8d177ec74fca8992
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-07 17:19:00 +0530

    added test page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Test from './Test';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Test />, document.getElementById('root'));
