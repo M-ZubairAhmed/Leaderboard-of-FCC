f8223612036b87e3c7000a08b749d69554a9ecbe
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-07 17:26:43 +0530

    added test case for large text

--- src/Test.js
+++ src/Test.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Photo from './components/photo/Photo';
+import LargeText from './components/largeText/LargeText';
 
 export default class Test extends Component {
   render() {
     return (
       <div>
         <div>
-          <h4>Photo component</h4>
+          <i>Photo component</i>
           <Photo imageUrl="https://goo.gl/6qpk6j" />
+        </div>
+        <div>
+          <LargeText text="LargeText : Sherlock Holmes" />
         </div>
       </div>
     );
