33587162ee61c41664b300002fcbffae576f7c3a
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-08 17:53:09 +0530

    added loader component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './loader.css';
+import PropTypes from 'prop-types';
+
+export default class Loader extends Component {
+  render() {
+    if (this.props.loadingStatus === 'initial') {
+      return <div className="loader" />;
+    } else if (this.props.loadingStatus === 'failed') {
+      return <div />;
+    } else {
+      return <div />;
+    }
+  }
+}
+
+Loader.propTypes = {
+  loadingStatus: PropTypes.oneOf(['initial', 'failed', 'done']).isRequired
+};
