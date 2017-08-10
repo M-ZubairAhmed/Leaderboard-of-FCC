2c3353ea66d5ab474ad5de85ec48e4a1b1f91306
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-10 14:44:20 +0530

    trying to make fetch in a different function for easy calling and made refresh icon

--- src/scenes/recentScores/RecentScoreBoard.js
+++ src/scenes/recentScores/RecentScoreBoard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Loader from '../../components/loader/Loader';
 import Row from '../../components/row/Row';
+import { Icon } from 'semantic-ui-react';
 import moment from 'moment';
 
 export default class RecentScoreBoard extends Component {
@@ -8,8 +9,10 @@ export default class RecentScoreBoard extends Component {
     super(props);
     this.state = {
       loadingStatus: 'initial',
-      dataFromNetwork: ''
+      dataFromNetwork: '',
+      refreshProgress: false
     };
+    // this.refreshScores = this.refreshScores.bind(this);
   }
 
   async componentDidMount() {
@@ -38,6 +41,36 @@ export default class RecentScoreBoard extends Component {
       this.setState({
         loadingStatus: fetchStatus
       });
+    }
+  }
+
+  async downloadJSON() {
+    let fetchStatus = 'done';
+    let responseJson = '';
+    try {
+      const response = await fetch(
+        'https://fcctop100.herokuapp.com/api/fccusers/top/recent'
+      );
+      // Check if theres a successfull network call
+      if (response.ok) {
+        responseJson = await response.json();
+        // this.setState({
+        //   dataFromNetwork: responseJson
+        // });
+        return await responseJson;
+      } else {
+        console.log('Server responded with message', response.status);
+        fetchStatus = 'failed';
+      }
+    } catch (e) {
+      console.log(e.message);
+      fetchStatus = 'failed';
+      return await fetchStatus;
+    } finally {
+      console.log('Fetching of Data', fetchStatus);
+      // this.setState({
+      //   loadingStatus: fetchStatus
+      // });
     }
   }
 
@@ -72,10 +105,40 @@ export default class RecentScoreBoard extends Component {
     return moment(lastUpdate).fromNow();
   };
 
+  refreshScores = async () => {
+    this.setState({
+      refreshProgress: true
+    });
+    const updatedJSON = await this.downloadJSON();
+    if (
+      this.state.dataFromNetwork !== '' &&
+      this.state.loadingStatus === 'done' &&
+      updatedJSON !== 'failed'
+    ) {
+      const updatedJSON_Stringi = JSON.stringify(updatedJSON);
+      const previousJSON_Stringi = JSON.stringify(this.state.dataFromNetwork);
+      if (updatedJSON_Stringi === previousJSON_Stringi) {
+        console.log('no change');
+      }
+    } else {
+      console.log('error');
+    }
+    this.setState({
+      refreshProgress: false
+    });
+  };
+
   render() {
     // console.log('rendering');
     return (
       <div>
+        <button onClick={this.refreshScores}>Refresh</button>
+        <Icon
+          loading={this.state.refreshProgress}
+          name="repeat"
+          onClick={this.refreshScores}
+          link
+        />
         {this.renderIndividualRows()}
         <Loader loadingStatus={this.state.loadingStatus} />
       </div>
