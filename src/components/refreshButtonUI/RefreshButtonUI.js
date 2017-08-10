a06d1bcccae18b8b26f41ee2f521d33c9ea93462
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-10 23:33:27 +0530

    wrote initial code for new component , refreshButton

---
+++ src/components/refreshButtonUI/RefreshButtonUI.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react';
+import moment from 'moment';
+
+let intervalID = '';
+let startTime = moment().toISOString();
+export default class RefreshButtonUI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elapsedRefreshTime: ''
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.shouldRefreshTimerStart === true) {
+      intervalID = this.startInterval();
+    }
+  }
+
+  startInterval = () => {
+    return setInterval(() => this.startElapsedRefreshTime(), 30000);
+  };
+
+  startElapsedRefreshTime = () => {
+    const elapsedRefreshTime = moment(startTime).fromNow();
+    this.setState({
+      elapsedRefreshTime: elapsedRefreshTime
+    });
+  };
+
+  resetElapsedRefreshTime = () => {
+    clearInterval(intervalID);
+    startTime = moment().toISOString();
+    this.props.onClick();
+  };
+
+  render() {
+    return (
+      <div onClick={this.resetElapsedRefreshTime}>
+        <Icon loading={this.props.loading} name="repeat" link />
+        <div>
+          {this.props.lastUpdated}
+        </div>
+        <div>
+          {this.state.elapsedRefreshTime}
+        </div>
+      </div>
+    );
+  }
+}
